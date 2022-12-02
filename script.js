a0814ea39ddc10002bbd0f61abdd6402be14ffb5
Roger Earl <rogerearl@me.com>
2022-12-01 23:42:47 -0800

    Added ball

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-const canvas = document.getElementById("playScreen");
+const canvas = document.getElementById("playArea");
 const con = canvas.getContext("2d");
+
+
+con.beginPath();
+con.arc(150, 100, 3, 0, Math.PI * 2, false);
+con.fillStyle = 'blue';
+con.fill();
+con.closePath;
