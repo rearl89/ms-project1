a403a3bb942ae87df2e3901fdcc5ce6cd1d70357
Roger Earl <rogerearl@me.com>
2022-12-02 23:39:49 -0800

    Added ball movement. Changed ball color

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const canvas = document.getElementById("playArea");
 const con = canvas.getContext("2d");
 
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
 
-con.beginPath();
-con.arc(150, 100, 3, 0, Math.PI * 2, false);
-con.fillStyle = 'blue';
-con.fill();
-con.closePath;
+function drawBall() {
+    con.beginPath();
+    con.arc(x, y, 3, 0, Math.PI * 2);
+    con.fillStyle = 'aqua';
+    con.fill();
+    con.closePath();
+}
+
+function draw() {
+    con.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall()
+    x += dx;
+    y += dy;
+}
+setInterval(draw, 10);
