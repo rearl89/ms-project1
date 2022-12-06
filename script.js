bf0ef7101e72bfe3b5451f794640e58b27b08c9c
Roger Earl <rogerearl@me.com>
2022-12-05 18:51:17 -0800

    added another row of bricks and changed colors

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+//create canvas
 const cvs = document.getElementById("playArea");
 const con = cvs.getContext("2d");
 
 //ball size
 const ballRadius = 3;
+
 //paddle size and position
 const paddleHeight = 5;
 const paddleWidth = 45;
 const paddleStartHeight = 8;
-let paddleX = (cvs.width - paddleWidth) / 2;
+let paddleXPosition = (cvs.width - paddleWidth) / 2;
+
 //bricks size
 const brickHeight = 8;
 const brickWidth = 25;
+
 //controls
 let rightPressed = false;
 let leftPressed = false;
@@ -52,39 +56,163 @@ function drawBall() {
 //draw paddle
 function drawPaddle(){
     con.beginPath();
-    con.rect(paddleX, cvs.height - paddleStartHeight, paddleWidth, paddleHeight);
+    con.rect(paddleXPosition, cvs.height - paddleStartHeight, paddleWidth, paddleHeight);
     con.fillStyle = '#CFD0D2';
     con.fill();
     con.closePath();
 }
 
+
+
 //draw bricks
 function drawBricks() {
     con.beginPath();
     con.rect(14, 3, brickWidth, brickHeight);
-    con.rect(49, 3, brickWidth, brickHeight);
-    con.rect(84, 3, brickWidth, brickHeight);
-    con.rect(119, 3, brickWidth, brickHeight);
-    con.rect(154, 3, brickWidth, brickHeight);
-    con.rect(189, 3, brickWidth, brickHeight);
-    con.rect(224, 3, brickWidth, brickHeight);
-    con.rect(259, 3, brickWidth, brickHeight);
-    con.rect(32, 15, brickWidth, brickHeight);
-    con.rect(67, 15, brickWidth, brickHeight);
-    con.rect(102, 15, brickWidth, brickHeight);
-    con.rect(137, 15, brickWidth, brickHeight);
-    con.rect(172, 15, brickWidth, brickHeight);
-    con.rect(207, 15, brickWidth, brickHeight);
-    con.rect(242, 15, brickWidth, brickHeight);
-    con.rect(14, 27, brickWidth, brickHeight);
-    con.rect(49, 27, brickWidth, brickHeight);
-    con.rect(84, 27, brickWidth, brickHeight);
-    con.rect(119, 27, brickWidth, brickHeight);
-    con.rect(154, 27, brickWidth, brickHeight);
-    con.rect(189, 27, brickWidth, brickHeight);
-    con.rect(224, 27, brickWidth, brickHeight);
-    con.rect(259, 27, brickWidth, brickHeight);
     con.fillStyle = 'red';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(49, 3, brickWidth, brickHeight);
+    con.fillStyle = 'red';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(84, 3, brickWidth, brickHeight);
+    con.fillStyle = 'red';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(119, 3, brickWidth, brickHeight);
+    con.fillStyle = 'red';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(154, 3, brickWidth, brickHeight);
+    con.fillStyle = 'red';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(189, 3, brickWidth, brickHeight);
+    con.fillStyle = 'red';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(224, 3, brickWidth, brickHeight);
+    con.fillStyle = 'red';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(259, 3, brickWidth, brickHeight);
+    con.fillStyle = 'red';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(32, 15, brickWidth, brickHeight);
+    con.fillStyle = 'yellow';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(67, 15, brickWidth, brickHeight);
+    con.fillStyle = 'yellow';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(102, 15, brickWidth, brickHeight);
+    con.fillStyle = 'yellow';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(137, 15, brickWidth, brickHeight);
+    con.fillStyle = 'yellow';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(172, 15, brickWidth, brickHeight);
+    con.fillStyle = 'yellow';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(207, 15, brickWidth, brickHeight);
+    con.fillStyle = 'yellow';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(242, 15, brickWidth, brickHeight);
+    con.fillStyle = 'yellow';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(14, 27, brickWidth, brickHeight);
+    con.fillStyle = '#FF00FF';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(49, 27, brickWidth, brickHeight);
+    con.fillStyle = '#FF00FF';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(84, 27, brickWidth, brickHeight);
+    con.fillStyle = '#FF00FF';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(119, 27, brickWidth, brickHeight);
+    con.fillStyle = '#FF00FF';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(154, 27, brickWidth, brickHeight);
+    con.fillStyle = '#FF00FF';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(189, 27, brickWidth, brickHeight);
+    con.fillStyle = '#FF00FF';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(224, 27, brickWidth, brickHeight);
+    con.fillStyle = '#FF00FF';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(259, 27, brickWidth, brickHeight);
+    con.fillStyle = '#FF00FF';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(32, 39, brickWidth, brickHeight);
+    con.fillStyle = '#16FC10';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(67, 39, brickWidth, brickHeight);
+    con.fillStyle = '#16FC10';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(102, 39, brickWidth, brickHeight);
+    con.fillStyle = '#16FC10';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(137, 39, brickWidth, brickHeight);
+    con.fillStyle = '#16FC10';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(172, 39, brickWidth, brickHeight);
+    con.fillStyle = '#16FC10';
+    con.fill();
+    con.closePath();
+    con.beginPath();
+    con.rect(207, 39, brickWidth, brickHeight);
+    con.fillStyle = '#16FC10';
+    con.fill();
+    con.closePath();
+    con.rect(242, 39, brickWidth, brickHeight);
+    con.fillStyle = '#16FC10';
     con.fill();
     con.closePath();
 }
@@ -93,27 +221,28 @@ function drawBricks() {
 function draw() {
     con.clearRect(0, 0, cvs.width, cvs.height); //clears ball trail
     drawBall();
-    if(x + dx > cvs.width - ballRadius || x + dx < ballRadius) {
+    x += dx;
+    y += dy;
+    if(x + dx > cvs.width - ballRadius || x + dx < ballRadius) { //ball bounce off walls
         dx = -dx;
     }else if(y + dy < ballRadius) {
         dy = -dy;
-    }else if(y + dy > cvs.height - ballRadius - paddleHeight) {
-        if(x > paddleX && x < paddleX + paddleWidth) {
+    }else if(y + dy > (cvs.height - ballRadius) - paddleHeight) { //ball bounce off paddle
+        if(x > paddleXPosition && x < paddleXPosition + paddleWidth) {
             dy = -dy;
         }else{
-        document.location.reload();
+        //document.location.reload(); //reset game if ball misses paddle
         clearInterval(interval);
         }
     }
 
     drawPaddle();
     if(rightPressed) {
-        paddleX = Math.min(paddleX + 4, cvs.width - paddleWidth);
+        paddleXPosition = Math.min(paddleXPosition + 4, cvs.width - paddleWidth);
     }else if (leftPressed) {
-        paddleX = Math.max(paddleX - 4, 0);
+        paddleXPosition = Math.max(paddleXPosition - 4, 0);
     }
-    drawBricks()
-    x += dx;
-    y += dy;
+   drawBricks();
+
 }
 const interval = setInterval(draw, 10);
